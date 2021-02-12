e66e79113282e129b7c2c8ffd69b1c702102cb11
yamutya <ksw2070374@stu.o-hara.ac.jp>
2021-02-12 13:54:00 +0900

    初期設定

---
+++ customer_output.php
@@ -0,0 +1,17 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>会員登録画面</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+	<?php require 'menu.php'; ?>
+
+</body>
+
+</html>
